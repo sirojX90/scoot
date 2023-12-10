0e90e58075ef2db5fe5b62588360274c2a3e22b5
Sirojiddin Xidirnazarov <sirojiddinx1990@gmail.com>
2023-12-10 12:57:51 +0500

    not found page done

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,5 +1,17 @@
+import { Footer } from "../../components/layout/footer"
+import { Navbar } from "../../components/layout/navbar/Navbar"
+
 export const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="container">
+      <Navbar/>
+      <div className="flex justify-center items-center px-[165px]">
+        <h2 className="text-[50px] text-center font-bold font-[SpaceMono] my-[300px]">
+          404 error <br />
+          Page not found
+        </h2>
+      </div>
+      <Footer/>
+    </div>
   )
 }
